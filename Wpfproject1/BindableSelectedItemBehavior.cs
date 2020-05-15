4a38291ac453a63706bd937528eca40ede03be4c
sina fayazi <sinafayazi@yahoo.com>
2020-05-15 12:31:39 +0430

    1

--- Wpfproject1/BindableSelectedItemBehavior.cs
+++ Wpfproject1/BindableSelectedItemBehavior.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using Microsoft.Xaml.Behaviors;
 using System.Windows;
 using System.Windows.Navigation;
 
